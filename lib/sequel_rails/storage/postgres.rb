75d6b417ccd5b030ae6e6e7dd52e2c5b7d218479
Sascha Cunz <sascha@babbelbox.org>
2013-01-15 04:31:27 +0100

    Convert port number to string

--- lib/sequel_rails/storage/postgres.rb
+++ lib/sequel_rails/storage/postgres.rb
@@ -6,7 +6,7 @@ module SequelRails
         commands = ["createdb", "--encoding", charset]
         commands << "--username" << username unless username.blank?
         commands << "--owner" << owner unless owner.blank?
-        commands << "--port" << port unless port.blank?
+        commands << "--port" << port.to_s unless port.blank?
         commands << "--host" << host unless host.blank?
         commands << database
         res = system(*commands)
@@ -18,7 +18,7 @@ module SequelRails
         ENV["PGPASSWORD"] = password unless password.blank?
         commands = ["dropdb"]
         commands << "-U" << username unless username.blank?
-        commands << "--port" << port unless port.blank?
+        commands << "--port" << port.to_s unless port.blank?
         commands << "--host" << host unless host.blank?
         commands << database
         res = system(*commands)
