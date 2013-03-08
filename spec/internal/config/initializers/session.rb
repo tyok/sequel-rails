2f50e18612381abf794e600243bbb78367d846fd
Jonathan Tron <jonathan.tron@thetalentbox.com>
2013-03-08 11:24:18 +0100

    Another 1.9 has syntax...

--- spec/internal/config/initializers/session.rb
+++ spec/internal/config/initializers/session.rb
@@ -1,1 +1,1 @@
-Combustion::Application.config.session_store :cookie_store, key: "sequel-rails", secret: "secret-token"
+Combustion::Application.config.session_store :cookie_store, :key => "sequel-rails", :secret => "secret-token"
