3268e6f9431b8a2d8cb0dc8a2a92fadc11267889
Jonathan Tron <jonathan.tron@metrilio.com>
2013-07-23 11:27:05 +0200

    Fix spec on JRUBY 1.8 mode

--- spec/internal/db/migrate/1273253849_add_twitter_handle_to_users.rb
+++ spec/internal/db/migrate/1273253849_add_twitter_handle_to_users.rb
@@ -2,7 +2,7 @@ Sequel.migration do
 
   change do
     alter_table :users do
-      add_column :twitter_handle, String, text: true
+      add_column :twitter_handle, String, :text => true
     end
   end
 
