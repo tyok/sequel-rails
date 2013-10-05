5ab72afda67d76aa23711e51bdd262b4547e9623
Kevin Menard <nirvdrum@gmail.com>
2013-10-04 23:02:52 -0400

    Since we're using system commands anyway, delegate up to the Postgres handler so we invoke the commands with the correct arguments.

--- lib/sequel_rails/storage/jdbc.rb
+++ lib/sequel_rails/storage/jdbc.rb
@@ -35,7 +35,8 @@ module SequelRails
             db.execute("CREATE DATABASE IF NOT EXISTS `#{db_name}` DEFAULT CHARACTER SET #{charset} DEFAULT COLLATE #{collation}")
           end
         elsif _is_postgres?
-          system("createdb #{db_name}")
+          adapter = ::SequelRails::Storage::Postgres.new(config)
+          adapter._create
         end
       end
 
@@ -48,7 +49,8 @@ module SequelRails
             db.execute("DROP DATABASE IF EXISTS `#{db_name}`")
           end
         elsif _is_postgres?
-          system("dropdb #{db_name}")
+          adapter = ::SequelRails::Storage::Postgres.new(config)
+          adapter._drop
         end
       end
 
